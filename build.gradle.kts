7742f87e5c6cfaa27f7691b277dd5cd4b435808c
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2023-06-21 16:42:07 +0200

    Add an ability to use bootstrap version of Kotlin out of the box

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,7 @@ allprojects.forEach {
 repositories {
     mavenCentral()
     maven(uri("./kotlin-compiler"))
+    maven(uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/"))
 }
 
 val distinguishAttribute = Attribute.of("kotlinx-benchmark-distinguishAttribute", String::class.java)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,11 +4,13 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven(uri("./kotlin-compiler"))
+        maven(uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/"))
     }
     resolutionStrategy {
         repositories {
             maven(uri("./kotlin-compiler"))
             gradlePluginPortal()
+            maven(uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/"))
         }
     }
     val kotlin_version: String by settings
